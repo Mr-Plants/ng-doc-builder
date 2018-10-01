5af1a906728fa27e3385b1933fe370880808d6d1
weiyabo <weiyb@jiedaibao.com>
2018-10-01 19:54:36 +0800

    加入脚本

--- build_scripts/generate.js
+++ build_scripts/generate.js
@@ -3,12 +3,33 @@
  * @date 2018/9/30
  * @Description: 编译脚本
  */
-const marked = require('marked');
+
 const fs = require('fs-extra');
-const remark = require('remark')();
-const yfm = require('yaml-front-matter');
 const PrismAngular = require('./utils/angular-language-marked');
 const Prism = require('node-prismjs');
 const path = require('path');
+const nameWithoutSuffix = require('./utils/name-without-suffix');
+const parseMd2Html = require('./utils/parse-md2html');
 
-const documentDir = path.resolve(__dirname, '../document');
+const documentPath = path.resolve(__dirname, '../document');
+
+const temporarySrc = path.resolve(__dirname, '/src');
+
+const documentDir = fs.readdirSync(documentPath);
+documentDir.forEach(component => {
+    const componentDir = path.join(documentPath, component);
+    const demoDir = fs.readdirSync(path.join(componentDir, 'demo'));
+    const demoMap = {};
+    demoDir.forEach(file => {
+        const nameKey = nameWithoutSuffix(file);
+        const demoFile = readFileSync(path.resolve(__dirname, file));
+        if (/.md$/.test(file)) {
+            demoMap[nameKey] = parseMd2Html(demoFile);
+        } else if (/.ts$/.test(file)) {
+            const highlightCode = PrismAngular.highlight(demoFile, Prism.languages['angular']);
+            demoMap[nameKey].highlightCode = `<pre class="language-angular"><code class="language-angular">${highlightCode}</code></pre>`
+        }
+    })
+});
+// fs.removeSync('../src');
+// fs.copySync()
---
+++ build_scripts/utils/parse-md2html.js
@@ -0,0 +1,23 @@
+/**
+ * @author weiyb
+ * @date 2018/10/1
+ * @Description: 将md编译为html片段和meta信息
+*/
+const marked = require('marked');
+const fs = require('fs-extra');
+const remark = require('remark')();
+const yfm = require('yaml-front-matter');
+
+module.export = function parseMd2Html(file) {
+    const meta = yfm.loadFront(file);
+    let htmlContent = '';
+    const mdObject = remark.parse(meta.__content);
+    mdObject.children.forEach(mdChild => {
+        htmlContent += marked(remark.stringify(mdChild));
+    });
+
+    return {
+        meta,
+        htmlContent
+    }
+}
